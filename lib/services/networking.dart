306b51c7c26e8f03fe8e4da5b79e5bd98feb1781
tuanha15 <tuanha15@vpbank.com.vn>
2024-11-12 16:33:29 +0700

    first commit

---
+++ lib/services/networking.dart
@@ -0,0 +1,21 @@
+import 'package:dio/dio.dart';
+
+class NetworkHelper {
+  NetworkHelper(this.url);
+  final dio = Dio();
+
+  final String url;
+
+  Future<dynamic> getData() async {
+    final response = await dio.get(url);
+
+    if (response.statusCode == 200) {
+      final data = response.data;
+
+      return data;
+    } else {
+      print(response.statusCode);
+      return null;
+    }
+  }
+}
